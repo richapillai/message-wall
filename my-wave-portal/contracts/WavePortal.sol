47868c94bcb766589c876ee868421662ea9e6cca
richapillai <richapillai@uchicago.edu>
2022-07-27 16:14:14 -0400

    first contract done

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -3,10 +3,21 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-// import "../node_modules/hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
